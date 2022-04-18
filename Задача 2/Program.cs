6132f6a7158c48111c6ca93dd1bee0429efdc2b7
ArtemSpivak <spivak25@yandex.ru>
2022-04-18 22:36:03 +0300

    .\Program.cs

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿// Программа для нахождения длины отрезка между 2 точками в 3D
+double ax = Coordinate("x", "A");
+double ay = Coordinate("y", "A");
+double az = Coordinate("z", "A");
+double bx = Coordinate("x", "B");
+double by = Coordinate("y", "B");
+double bz = Coordinate("z", "A");
+
+double squareX = Math.Pow(bx-ax, 2);
+double squareY = Math.Pow(by-ay, 2);
+double squareZ = Math.Pow(bz-az, 2);
+double sqrt = Math.Sqrt(squareX+squareY+squareZ);
+
+Console.Write(sqrt);
+
+double Coordinate(string coorName, string pointName)
+{
+    Console.Write($"Введите координату {coorName} точки {pointName}:");
+    return Convert.ToDouble(Console.ReadLine());
+}
