e8feddac78e82f0e87276337104f55484ab9c4e4
ArtemSpivak <spivak25@yandex.ru>
2022-04-18 22:25:53 +0300

    Задача решена

--- Задача 3/Program.cs
+++ Задача 3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача на вывод кубов целых чисел
+Console.Write("Введите число:");
+double number = Convert.ToDouble(Console.ReadLine());
+double index = 1;
+
+while (index <= number)
+{
+    double result = Math.Pow(index, 3);
+    index +=1;
+    Console.Write($"{result} ");
+}
