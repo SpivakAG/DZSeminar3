52def4a0f537b4f09d2eeab16907f167c7c63bea
ArtemSpivak <spivak25@yandex.ru>
2022-04-18 22:48:12 +0300

    Задача решена

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Программа для проверки является ли пятизначное число паллиндромом
+Console.Write("Введите пятизначное число:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <10000 || number > 99999) Console.Write("Введеное число не является пятизначным");
+
+int reverse = ReverseNumber (number);
+
+if (number != reverse) Console.Write("Число не является палиндромом");
+else Console.Write("Число является палиндромом");
+
+
+int ReverseNumber (int n1)
+{
+    int result = 0;
+    while (n1 > 0)
+    {
+    result *= 10;
+    result += n1 % 10;
+    n1 /= 10;
+    }
+    return result;
+}
